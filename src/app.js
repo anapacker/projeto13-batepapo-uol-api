b3e6e92df1dfdfba7224d6f1cb3e1edca71f2282
anapacker <anaamaria.packer@gmail.com>
2023-06-30 00:11:31 -0300

    pegando lista de menssagens

--- src/app.js
+++ src/app.js
@@ -55,14 +55,20 @@ app.get("/participants", async (req, res) => {
 
 app.post("/messages", async (req, res) => {
     const { to, text, type } = req.body
-    const { from } = req.headers.user
 
     if (!to || !text || !type) {
         return res.sendStatus(422)
     }
 
+    const { from } = req.headers.user
+    const messagesSchema = joi.object({
+        to: joi.string().required(),
+        text: joi.string().required(),
+        type: joi.valid('message, private_message').required
+    })
+
     try {
-        const participantExists = await db.collection("messages").findOne({ name: from })
+        const participantExists = await db.collection("participants").findOne({ name: from })
         if (!participantExists) {
             res.sendStatus(422)
         }
@@ -74,13 +80,15 @@ app.post("/messages", async (req, res) => {
 }
 )
 
-// })
-// app.get("/messages", (req, res) => {
-//     const promisse = db.collection("messages").find().toArray()
-//     promisse.then(names => res.send(names))
-//     promisse.catch(err => res.status())
+app.get("/messages", async (req, res) => {
+    try {
+        const messages = await db.collection("messages").find().toArray()
+        res.send(messages)
+    } catch (err) {
+        res.status(500).send(err.message)
+    }
 
-// })
+})
 
 
 const PORT = 5000
